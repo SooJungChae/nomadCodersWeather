c22e8c5442cc6329b0a1ae0e3e5617c3f62bba92
SoojungChae <naan_ace@naver.com>
2019-08-14 12:01:19 +0900

    Add loading text if API is unloaded
    Add loading text with if condition codes
    Delete unusing codes

--- App.js
+++ App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-	    <View style={styles.redView} />
-	    <View style={styles.yellowView}/>
-    </View>
-  );
+export default class App extends Component {
+  state = {
+    isLoaded: false
+  };
+
+  render() {
+    const { isLoaded } = this.state;
+    return (
+      <View style={styles.container}>
+        {isLoaded ? null : (
+          <View style={styles.loading}>
+            <Text style={styles.loadingText}>Getting the weather</Text>
+          </View>
+        )}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff"
   },
-  redView: {
-    flex: 1,
-    backgroundColor: 'red'
-  },
-	yellowView: {
-		flex: 6,
-		backgroundColor: 'yellow'
+	loading: {
+  	flex: 1,
+  	backgroundColor: "#FDF6AA",
+		justifyContent: "flex-end",
+		paddingLeft: 25,
+		paddingRight: 25
+	},
+	loadingText: {
+  	fontSize: 38,
+		marginBottom: 100
 	}
 });
